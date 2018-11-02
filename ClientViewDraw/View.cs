0a61867bea4c7dccfa46d67dc4f4fbe35d12f7d5
Johannes Andersen <defe@live.dk>
2018-11-02 10:40:36 +0100

    SlaveClient

--- ClientViewDraw/View.cs
+++ ClientViewDraw/View.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Text;
+using System.Threading;
 
 namespace ClientViewDraw
 {
     static class View
     {
+        private static TcpClient slaveClient;
+        private static StreamReader sReader;
+        private static StreamWriter sWriter; //Maybe if we want to make a game
+        private static bool connected;
+        private static int portNumber = 2565;
+        private static string[] cords;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -17,6 +29,35 @@ namespace ClientViewDraw
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new ViewForm());
+            ClientSetUp();
+        }
+
+        private static void ClientSetUp()
+        {
+            slaveClient = new TcpClient();
+            slaveClient.Connect(IPAddress.Any, portNumber);//Lets the client connect.
+            GetDrawing();
+        }
+        private static void GetDrawing()
+        {
+            NetworkStream getStream = slaveClient.GetStream();//Gets info from client
+            sReader = new StreamReader(getStream, Encoding.UTF8);
+            sWriter = new StreamWriter(getStream, Encoding.UTF8);
+
+            connected = true;
+
+            while (connected)
+            {
+                try
+                {
+
+                }
+                catch(Exception e)
+                {
+                    Thread.CurrentThread.Abort();
+                }
+            }
+
         }
     }
 }
